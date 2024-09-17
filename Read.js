1e805def90d611d41b7fcea0e87c8a4621003ff0
Soham kundu <151546383+code-with-soham@users.noreply.github.com>
2024-09-17 20:11:06 +0530

    Add files via upload

---
+++ Read.js
@@ -0,0 +1,92 @@
+    /*        <!-- 🔴 Variables --> *
+
+    * name = "Soham"
+    * age = 21
+    * University = "Brainware University"
+
+              <!-- 🔴 keywords -->
+
+      * for
+      * if
+      * else
+      * switch
+      * case
+      * default
+
+              <!-- 🔴 Data  Types -->
+
+    * string ="Soham kundu"; 
+    * number  = 21124, 30/44;
+    * boolean = True, False
+    * object = {name:"Soham", age:21}           
+    * array = [1,2,3,4,5];
+
+    <!-- 🔴 Var, Let, Const -->
+
+*✅ var:
+      var name = "Soham";
+    //  # only use var when you use Old Software .... 
+
+   
+/*     
+*✅ Let:
+    # let name = "Soham";
+    # let is used in modern software 
+
+     // variable defined with let must be declared before use
+    let a = 10;
+    console.log(a)
+
+    // Variavle defined with let have block scope
+    {
+    let string = "apple";
+    console.log(string)
+    console.log(a)
+    
+    }  
+/*
+*✅ Const:
+    # const name = "Soham";
+    # const is used when you want to declare a variable that should not be changed 
+
+    // variable defined with const must be declared before use
+    const constant = "mango";
+    console.log(constant)
+
+    // Variable defined with const have block scope
+    {
+    const constant = "mango";
+    console.log(constant)
+    }  
+    
+
+
+//            <!-- 🔴 Hoisting -->
+
+      am=1;
+      alert("am="+am);
+      var am;
+
+      
+//          <!-- 🔴 Types in JS -->
+
+//        Primitives: string, number, boolean, undefined, null, symbol 
+
+     let apple=[141,23,45];
+     let b=apple;
+     b.pop()
+    console.log("a =",apple)
+    console.log("b =",b)
+    
+
+        Reference: object, function
+                   {} , [], () */
+
+// Find Maxium among three variable       
+let a = 10;
+let b = 20;
+let c = 18;
+//  console.log(Math.max(a,b,c))
+a>b && a>c
+console.log(a)
+
